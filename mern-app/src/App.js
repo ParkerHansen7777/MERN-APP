aac7a2340c95d2d709ce8c149dd711188eaa98c7
ParkerHansen7777 <parkerhansen7777@gmail.com>
2021-10-30 10:35:30 -0700

    CSS updates to UI (in progress)

--- mern-app/src/App.js
+++ mern-app/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Sidebar from "./components/sidebar.component";
 import Navbar from "./components/navbar.component";
 import GamesList from "./components/games-list.component";
 import EditGame from "./components/edit-games.component";
 import CreateGame from "./components/create-game.component";
 import CreateUser from "./components/create-user.component";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="container">
+        <Sidebar />
         <Navbar />
         <br/>
         <Route path="/" exact component={GamesList} />
